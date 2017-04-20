1cdc6888ed6ddcdaccaf0f28e295cbc547bbe956
Elena Mengual Mesa <menme@alumni.uv.es>
2017-04-20 11:48:08 +0200

    Bookmarks France, Uk, Germany and Italy

---
+++ Spiegel/static/script.js
@@ -0,0 +1,33 @@
+  var win    = $( this );
+  var width  = api.tool.desktopWidth();
+  var height = api.tool.desktopHeight() - 70;
+  var left   = ( wz.tool.environmentWidth() / 2 ) - ( width / 2 );
+  var top    = ( wz.tool.environmentHeight() / 2 ) - ( height / 2 );
+  var windowObject = api.popup( 'http://www.spiegel.de/', width, height).render();
+
+    var timer = setInterval( function(){
+
+        if( windowObject.closed ){
+
+            wz.view.remove();
+            clearInterval( timer );
+
+        }
+
+    }, 500 );
+
+    win
+    .on( 'ui-view-focus', function(){
+        windowObject.focus();
+    })
+
+    .on( 'ui-view-removed', function(){
+        windowObject.close();
+    });
+
+    // To Do -> A la espera de métodos para conocer cuando se va a cerrar Inevio
+    /*
+    wzWindow.onbeforeunload = function(){
+        windowObject.close();
+    };
+    */
