68e4dfcd2befa51d622c64aafda5d705ecc028a6
Elena Mengual Mesa <menme@alumni.uv.es>
2017-04-20 10:00:02 +0200

    Init Commit

---
+++ Social Media/Twitter/static/script.js
@@ -0,0 +1,48 @@
+
+  var win    = $( this );
+  var width;
+  var height;
+  var minWidth  = 1000;
+  var minHeight = 629;
+  var maxWidth = api.tool.desktopWidth();
+  var maxHeigth = api.tool.desktopHeight() - 70;
+
+
+  if (minHeight > maxHeigth || minWidth > maxWidth) {
+    width = maxWidth;
+    height = maxHeigth;
+
+  }else {
+    width = minWidth;
+    height = minHeight;
+  }
+  var left   = ( wz.tool.environmentWidth() / 2 ) - ( width / 2 );
+  var top    = ( wz.tool.environmentHeight() / 2 ) - ( height / 2 );
+  var windowObject = api.popup( 'https://twitter.com/', width, height).render();
+
+    var timer = setInterval( function(){
+
+        if( windowObject.closed ){
+
+            wz.view.remove();
+            clearInterval( timer );
+
+        }
+
+    }, 500 );
+
+    win
+    .on( 'ui-view-focus', function(){
+        windowObject.focus();
+    })
+
+    .on( 'ui-view-removed', function(){
+        windowObject.close();
+    });
+
+    // To Do -> A la espera de métodos para conocer cuando se va a cerrar Inevio
+    /*
+    wzWindow.onbeforeunload = function(){
+        windowObject.close();
+    };
+    */
