b121c2d26749e66c2a41f06b066ab789c8713968
Carlos Navas <carlosnavas1991@gmail.com>
2017-04-26 16:34:31 +0200

    Added Amazon and AS

---
+++ Amazon/static/script.js
@@ -0,0 +1,43 @@
+var win    = $( this );
+
+var minWidth  = 1089;
+var minHeight = 688;
+var maxWidth = api.tool.desktopWidth() - 10;
+var maxHeigth = api.tool.desktopHeight() - 70;
+if (minHeight > maxHeigth || minWidth > maxWidth) {
+  width = maxWidth;
+  height = maxHeigth;
+
+}else {
+  width = minWidth;
+  height = minHeight;
+}
+
+var windowObject = api.popup( 'https://amazon.com/', width, height).render();
+
+  var timer = setInterval( function(){
+
+      if( windowObject.closed ){
+
+          wz.view.remove();
+          clearInterval( timer );
+
+      }
+
+  }, 500 );
+
+  win
+  .on( 'ui-view-focus', function(){
+      windowObject.focus();
+  })
+
+  .on( 'ui-view-removed', function(){
+      windowObject.close();
+  });
+
+  // To Do -> A la espera de métodos para conocer cuando se va a cerrar Inevio
+  /*
+  wzWindow.onbeforeunload = function(){
+      windowObject.close();
+  };
+  */
