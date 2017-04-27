0e4b916dc652ad9f7d41e52f3a832ff4b32ddd6b
Carlos Navas <carlosnavas1991@gmail.com>
2017-04-27 10:53:04 +0200

    Updated Instagram

--- Social Media/Instagram/static/script.js
+++ Social Media/Instagram/static/script.js
@@ -1,48 +1,43 @@
+var win    = $( this );
 
-  var win    = $( this );
-  var width;
-  var height;
-  var minWidth  = 720;
-  var minHeight = 629;
-  var maxWidth = api.tool.desktopWidth();
-  var maxHeigth = api.tool.desktopHeight() - 70;
+var minWidth  = 720;
+var minHeight = 630;
+var maxWidth = api.tool.desktopWidth() - 10;
+var maxHeigth = api.tool.desktopHeight() - 70;
+if (minHeight > maxHeigth || minWidth > maxWidth) {
+  width = maxWidth;
+  height = maxHeigth;
 
+}else {
+  width = minWidth;
+  height = minHeight;
+}
 
-  if (minHeight > maxHeigth || minWidth > maxWidth) {
-    width = maxWidth;
-    height = maxHeigth;
+var windowObject = api.popup( 'https://instagram.com/', width, height).render();
 
-  }else {
-    width = minWidth;
-    height = minHeight;
-  }
-  var left   = ( wz.tool.environmentWidth() / 2 ) - ( width / 2 );
-  var top    = ( wz.tool.environmentHeight() / 2 ) - ( height / 2 );
-  var windowObject = api.popup( 'https://instagram.com/', width, height).render();
+  var timer = setInterval( function(){
 
-    var timer = setInterval( function(){
+      if( windowObject.closed ){
 
-        if( windowObject.closed ){
+          wz.view.remove();
+          clearInterval( timer );
 
-            wz.view.remove();
-            clearInterval( timer );
+      }
 
-        }
+  }, 500 );
 
-    }, 500 );
+  win
+  .on( 'ui-view-focus', function(){
+      windowObject.focus();
+  })
 
-    win
-    .on( 'ui-view-focus', function(){
-        windowObject.focus();
-    })
+  .on( 'ui-view-removed', function(){
+      windowObject.close();
+  });
 
-    .on( 'ui-view-removed', function(){
-        windowObject.close();
-    });
-
-    // To Do -> A la espera de métodos para conocer cuando se va a cerrar Inevio
-    /*
-    wzWindow.onbeforeunload = function(){
-        windowObject.close();
-    };
-    */
+  // To Do -> A la espera de métodos para conocer cuando se va a cerrar Inevio
+  /*
+  wzWindow.onbeforeunload = function(){
+      windowObject.close();
+  };
+  */
