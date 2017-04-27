0d1ea151ca278da7969c3712062e7bb3c33e82e8
Jorge Fernandez <jorge92f@gmail.com>
2017-04-27 11:14:10 +0200

    UPDATE

--- Multimedia/Streaming/Twitch/static/script.js
+++ Multimedia/Streaming/Twitch/static/script.js
@@ -1,8 +1,16 @@
   var win    = $( this );
-  var width  = api.tool.desktopWidth();
-  var height = api.tool.desktopHeight() - 70;
-  var left   = ( wz.tool.environmentWidth() / 2 ) - ( width / 2 );
-  var top    = ( wz.tool.environmentHeight() / 2 ) - ( height / 2 );
+  var minWidth  = 1050;
+  var minHeight = 700;
+  var maxWidth = api.tool.desktopWidth() - 10;
+  var maxHeigth = api.tool.desktopHeight() - 70;
+  if (minHeight > maxHeigth || minWidth > maxWidth) {
+    width = maxWidth;
+    height = maxHeigth;
+
+  }else {
+    width = minWidth;
+    height = minHeight;
+  }
   var windowObject = api.popup( 'https://www.twitch.tv/', width, height).render();
 
     var timer = setInterval( function(){
