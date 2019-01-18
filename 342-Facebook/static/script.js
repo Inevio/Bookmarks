84e2093b1144067c6fe101bc3c7bce10e27019b5
Carlos Navas <carlosnavas1991@gmail.com>
2019-01-18 11:20:46 +0100

    Added badge to FB

--- 342-Facebook/static/script.js
+++ 342-Facebook/static/script.js
@@ -1,1 +1,20 @@
-console.log('estoy abriendo facebook')
+let regex = /^\((\d+)\)/
+
+setTimeout(function(){
+
+  let webviewDom = $('.browser-content .webview-0')
+
+  webviewDom.on('page-title-updated', function(event){
+    event = event.originalEvent
+    let title = event.title
+    let myRegex = new RegExp(regex)
+    let match = myRegex.exec(title)
+    console.log('cambio de titulo ',title, match)
+    if(match && match[1]){
+      api.app.setBadge(match[1])
+    }else{
+      api.app.setBadge(0)
+    }
+  })
+
+},500)
