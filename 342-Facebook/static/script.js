87859d56ffb97e0c5a883b5953d1e65e936f621a
Carlos Navas <carlosnavas1991@gmail.com>
2019-02-01 11:28:48 +0100

    Added badge to FB

--- 342-Facebook/static/script.js
+++ 342-Facebook/static/script.js
@@ -1,20 +1,11 @@
-let regex = /^\((\d+)\)/
-
 setTimeout(function(){
 
   let webviewDom = $('.browser-content .webview-0')
-
-  webviewDom.on('page-title-updated', function(event){
-    event = event.originalEvent
-    let title = event.title
-    let myRegex = new RegExp(regex)
-    let match = myRegex.exec(title)
-    console.log('cambio de titulo ',title, match)
-    if(match && match[1]){
-      api.app.setBadge(match[1])
-    }else{
-      api.app.setBadge(0)
+  let interval = setInterval(function(){
+    if(!webviewDom[0].getWebContents()){
+      clearInterval(interval)
     }
-  })
+    webviewDom[0].getWebContents().send('getNotifications',  JSON.stringify(['._3eo9','._51lp']))
+  }, 2500)
 
 },500)
