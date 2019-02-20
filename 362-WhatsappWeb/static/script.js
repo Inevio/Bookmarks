d1bf3e69597872954f28d02e964227e9620b223a
Carlos Navas <carlosnavas1991@gmail.com>
2019-02-20 12:58:07 +0100

    Updated whatsapp web

---
+++ 362-WhatsappWeb/static/script.js
@@ -0,0 +1,36 @@
+let regex = /^\((\d+)\)\s.*/
+
+setTimeout(function(){
+
+  let webviewDom = $('.browser-content .webview-0')
+  let lastCount = 0
+
+  webviewDom.on('page-title-updated', function(event){
+    event = event.originalEvent
+    let title = event.title
+    let myRegex = new RegExp(regex)
+    let match = myRegex.exec(title)
+    let timeout
+    console.log('cambio de titulo ',title, match)
+    if(match && match[1]){
+      lastCount = parseInt(match[1])
+      api.app.setBadge(parseInt(match[1]))
+      if(timeout){
+        clearTimeout(timeout)
+      }
+    }else if(lastCount === 0){
+      api.app.setBadge(0)
+    }else{
+      timeout = setTimeout(function(){
+        if(!webviewDom[0]) return
+        let newTitle = webviewDom[0].getTitle()
+        let myRegex2 = new RegExp(regex)
+        let match2 = myRegex.exec(newTitle)
+        if(!(match2 && match2[1])){
+          api.app.setBadge(0)
+        }
+      },6000)
+    }
+  })
+
+},500)
