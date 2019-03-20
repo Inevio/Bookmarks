c223ef04aa0e90c27d3aaf8d0f1be033d68c3f3b
Carlos Navas <carlosnavas1991@gmail.com>
2019-03-20 13:45:46 +0100

    Updated FB messenger

--- 1084-Facebook Messenger/static/script.js
+++ 1084-Facebook Messenger/static/script.js
@@ -9,11 +9,18 @@ setTimeout(function(){
     let title = event.title
     let myRegex = new RegExp(regex)
     let match = myRegex.exec(title)
+    let refreshed = false
     console.log('cambio de titulo ',title, match)
     if(match && match[1]){
       api.app.setBadge(match[1])
+      refreshed = true
     }else{
-      api.app.setBadge(0)
+      refreshed = false
+      setTimeout( function(){
+        if(!refreshed){
+          api.app.setBadge(0)
+        }
+      },3000)
     }
   })
 
